94cfa0fe954639c3269c3e0df2eeb62e0315a1a0
Akshay Nagrale <akshaynagrale123@gmail.com>
2020-09-28 22:48:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private Button btn1, btn2, btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction= fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.constLayout, new StatusFragment());
+                fragmentTransaction.commit();
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.constLayout, new CallFragment());
+                fragmentTransaction.commit();
+
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.constLayout, new ChatFragment());
+                fragmentTransaction.commit();
+
+            }
+        });
+    }
+}
